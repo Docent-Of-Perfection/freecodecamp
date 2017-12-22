0824cdbc99de6a285ccd3c15f4a6e0005f85ac9d
Sean <me@ininger.me>
2017-12-21 17:35:48 -0800

    First Commit

---
+++ BasicAlgorithm/factorialize2.js
@@ -0,0 +1,13 @@
+
+//Most of my problems with this one were based on my lack of understanding basic mathematics
+
+function factorialize(num) {
+  
+answer = 1;
+  for(x=2;x<=num;x++){
+    answer= answer * x;
+    console.log(answer + " *= " + x)
+  }
+ return answer;
+}
+
